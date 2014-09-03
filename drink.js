9ff4012af7dc47239cbc8c50ca6453c065d7fd4c
brimatteng <btenggren20@gmail.com>
2014-09-03 17:13:47 -0400

    Simple rendering of rum and coke.

--- drink.js
+++ drink.js
@@ -1,3 +1,21 @@
 window.onload = function () {
-    var s = Snap(600, 600);
+    var s = Snap(1000,1000);
+
+    var glass = s.rect(10,10, 300,500);
+    glass.attr({
+        fill:'#90ceff',
+        stroke:'#4292ff',
+        strokeWidth: 2
+    });
+
+    var alcohol = s.rect(13,400, 294,100);
+    alcohol.attr({
+        fill:'#81201a'
+    });
+
+    var mixer = s.rect(13,50, 294,350);
+    mixer.attr({
+        fill:'#2a0415'
+    });
+
 };
